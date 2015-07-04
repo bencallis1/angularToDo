cb792c1b8dd7d5868d372e099a2368c392c4e620
bencallis1 <bencallis1@gmail.com>
2015-07-03 23:57:50 -0600

    made changes to controller to add firebase constant

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,30 @@
 'use strict';
 
 angular
-  .module('angularToDoApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-      'ui.sortable'
-  ])
+  .module('angularToDoApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'ui.sortable',
+      "firebase"])
+
+    .filter('todoFilter', function ($location) {
+      return function (input) {
+        var filtered = {};
+        angular.forEach(input, function (todo, id) {
+          var path = $location.path();
+          if (path === '/active') {
+            if (!todo.completed) {
+              filtered[id] = todo;
+            }
+          } else if (path === '/completed') {
+            if (todo.completed) {
+              filtered[id] = todo;
+            }
+          } else {
+            filtered[id] = todo;
+          }
+        });
+        return filtered;
+      };
+    })
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
